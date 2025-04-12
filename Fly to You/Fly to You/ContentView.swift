fe377c52a0d709b0fd06146c59df7f6d4c119458
heejinnn <gml0593@gmail.com>
2025-04-12 23:11:17 +0900

    feat: cocoapod 설치 및 firebase 연동

---
+++ Fly to You/Fly to You/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fly to You
+//
+//  Created by 최희진 on 4/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fly to You/Fly to You/Fly_to_YouApp.swift
@@ -0,0 +1,22 @@
+//
+//  Fly_to_YouApp.swift
+//  Fly to You
+//
+//  Created by 최희진 on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct YourApp: App {
+
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
