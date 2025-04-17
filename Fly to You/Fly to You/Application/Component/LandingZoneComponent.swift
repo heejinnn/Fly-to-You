1c13cd1ea1fcd4eab8677153c69ea4b8433a218b
heejinnn <gml0593@gmail.com>
2025-04-17 22:42:21 +0900

    feat: LandingZone Factory 생성

---
+++ Fly to You/Fly to You/Application/Component/LandingZoneComponent.swift
@@ -0,0 +1,8 @@
+//
+//  LandingZoneComponent.swift
+//  Fly to You
+//
+//  Created by 최희진 on 4/17/25.
+//
+
+
--- Fly to You/Fly to You/Presentation/Factory/LandingZoneFactory.swift
+++ Fly to You/Fly to You/Presentation/Factory/LandingZoneFactory.swift
@@ -5,3 +5,32 @@
 //  Created by 최희진 on 4/17/25.
 //
 
+import SwiftUI
+
+// MARK: - MainFactoryDependency
+
+struct LandingZoneFactoryDependency: RootDependency {
+    let landingZoneFactory: any LandingZoneFactory
+}
+
+// MARK: - MainFactory
+
+protocol LandingZoneFactory {
+    associatedtype SomeView: View
+    func makeMainView() -> SomeView
+}
+
+// MARK: - DefaultMainFactory
+
+final class DefaultLandingZoneFactory: LandingZoneFactory {
+    private let landingZoneViewModelWrapper: LandingZoneViewModelWrapper
+
+    init(landingZoneViewModelWrapper: LandingZoneViewModelWrapper) {
+        self.landingZoneViewModelWrapper = landingZoneViewModelWrapper
+    }
+
+    public func makeMainView() -> some View { // some: "특정 타입만 반환"
+        return LandingZoneView()
+            .environmentObject(landingZoneViewModelWrapper)
+    }
+}
