835138c8ecb9ba2b1a00d15eab3bfb419a84a042
heejinnn <gml0593@gmail.com>
2025-04-22 23:32:05 +0900

    feat: Flight 모델 추가

---
+++ Fly to You/Fly to You/Domain/Model/Flight.swift
@@ -0,0 +1,13 @@
+//
+//  Flight.swift
+//  Fly to You
+//
+//  Created by 최희진 on 4/22/25.
+//
+
+struct Flight {
+    let id: String
+    let topic: String
+    let startDate: Date
+    let routes: [ReceiveLetter]
+}
