bb1dbd22e36b3b3363f17d6f48c1e34018a677dd
heejinnn <gml0593@gmail.com>
2025-08-16 18:34:37 +0900

    test: 필요없는 test code 제거

--- Fly to You/Fly to YouTests/DateUtilTest.swift
+++ Fly to You/Fly to YouTests/DateUtilTest.swift
@@ -24,35 +24,11 @@ final class DateUtilTest: XCTestCase {
         XCTAssertEqual(formattedDate, "2025.08.16")
     }
     
-    func test_letterDateFormatter_hasCorrectLocale() {
-        // When
-        let formatter = DateUtil.letterDateFormatter
-        
-        // Then
-        XCTAssertEqual(formatter.locale.identifier, "ko_KR")
-    }
-    
     func test_letterDateFormatter_hasCorrectDateFormat() {
         // When
         let formatter = DateUtil.letterDateFormatter
         
         // Then
         XCTAssertEqual(formatter.dateFormat, "yyyy.MM.dd")
-    }
-    
-    func test_formatLetterDate_multipleCallsWithSameDate_returnsSameResult() {
-        // Given
-        var calendar = Calendar(identifier: .gregorian)
-        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")!
-        let components = DateComponents(year: 2025, month: 6, day: 15)
-        let date = calendar.date(from: components)!
-        
-        // When
-        let formattedDate1 = DateUtil.convertToDateString(date)
-        let formattedDate2 = DateUtil.convertToDateString(date)
-        
-        // Then
-        XCTAssertEqual(formattedDate1, formattedDate2)
-        XCTAssertEqual(formattedDate1, "2025.06.15")
     }
 }
