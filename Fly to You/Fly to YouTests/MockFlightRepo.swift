187ba6da262dd209aa475d8469a84f51b8d106ba
heejinnn <gml0593@gmail.com>
2025-09-11 12:47:34 +0900

    refactor: 삭제된 파일 복구

---
+++ Fly to You/Fly to YouTests/MockFlightRepo.swift
@@ -0,0 +1,22 @@
+//
+//  MockFlightRepo.swift
+//  Fly to You
+//
+//  Created by 최희진 on 9/11/25.
+//
+
+@testable import Fly_to_You
+
+class DummyFlightRepo: FlightRepo {
+    func addRoute(flightId: String, letter: Fly_to_You.Letter) async throws {}
+    func deleteFlight(topicId: String) async throws {   }
+    func removeRoute(topicId: String, routeId: String) async throws {}
+    func updateRoute(letter: Fly_to_You.Letter) async throws {}
+    func getRoutes(topicId: String) async throws -> [[String : Any]] {
+        return [[:]]
+    }
+
+    func observeAllFlights(onUpdate: @escaping ([Fly_to_You.FlightDto]) -> Void) {}
+    func removeFlightsListener() {}
+
+}
